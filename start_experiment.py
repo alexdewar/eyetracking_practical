f396784b21f70fc4492cfe88375231fb89d0b93d
Alex Dewar <alex.dewar@gmx.co.uk>
2017-03-29 17:47:24 +0100

    change current directory before serving http requests - also add some comments

--- start_experiment.py
+++ start_experiment.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+from __future__ import print_function #python 3 print function
 
-LISTEN_PORT = 8000
+LISTEN_PORT = 8000 # port to serve http requests on
 
 import sys
+import os
 import subprocess
+
+# these modules are named differently in python 2 and 3
 if sys.version_info[0] > 2:
     import http.server as httpserver
     import socketserver
@@ -13,6 +16,7 @@ else:
     import SimpleHTTPServer as httpserver
     import SocketServer as socketserver
 
+# start the google chrome process
 print('Starting Google Chrome...')
 url = 'http://localhost:%d' % LISTEN_PORT;
 if sys.platform.startswith('linux'):
@@ -20,10 +24,12 @@ if sys.platform.startswith('linux'):
 elif sys.platform == 'win32':
     subprocess.Popen(['C:\Program Files (x86)\Google\Chrome\Application\chrome.exe', url])
 
+# change current directory to the directory this script is in
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# serve http requests to this folder
+socketserver.TCPServer.allow_reuse_address = True # this is needed to prevent an error when program running 2nd time
 handler = httpserver.SimpleHTTPRequestHandler
-
-socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(('localhost', LISTEN_PORT), handler)
-
 print('Starting webserver listening on port', LISTEN_PORT)
-httpd.serve_forever()
+httpd.serve_forever() # start serving requests
