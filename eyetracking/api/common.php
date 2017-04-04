ff6af689ab65a661a554f266eaa6470c0d772858
Alex Dewar <a.dewar@sussex.ac.uk>
2017-04-04 18:19:04 +0100

    fix: participant data json file name

--- eyetracking/api/common.php
+++ eyetracking/api/common.php
@@ -4,8 +4,8 @@ function p_filename($sid, $pid) {
     return sprintf('../data/s%04d_p%04d.json', $sid, $pid);
 }
 
-function p_data_filename($pid) {
-    return sprintf('../data/participant%04d_data.json', $pid);
+function p_data_filename($sid, $pid) {
+    return sprintf('../data/s%04d_%04d_data.json', $sid, $pid);
 }
 
 function p_error($error) {
