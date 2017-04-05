8a3e4a4f3f89233ceb7bba5b09e6b32cb13e642f
Alex Dewar <a.dewar@sussex.ac.uk>
2017-04-05 15:01:40 +0100

    option to show data for all participants
    i.e. mean of means (and CI for mean of means etc.)
    
    this is the default option

--- eyetracking/view_data.php
+++ eyetracking/view_data.php
@@ -19,6 +19,48 @@ function ci95($x, $mean) {
     return 1.96 * std($x, $mean) / sqrt(count($x));
 }
 
+function get_data($fn) {
+    $pdata = json_decode(file_get_contents($fn), true);
+
+    $eye_data = array();
+    foreach ($pdata['stimuli'] as $stim) {
+        $eye_data[$stim['name']]['src'] = $stim['src'];
+        $eye_data[$stim['name']]['dest'] = $stim['dest'];
+    }
+
+    $ctrial = '';
+    $stop = false;
+    foreach ($pdata['eye_data'] as $msg) {
+        switch ($msg['type']) {
+            case 'trial':
+                $ctrial = $msg['trial'];
+
+                $eye_data[$ctrial]['x'] = array();
+                $eye_data[$ctrial]['y'] = array();
+
+                break;
+            case 'rec':
+                if ($ctrial === '') {
+                    continue;
+                }
+
+                array_push($eye_data[$ctrial]['x'], $msg['x']);
+                array_push($eye_data[$ctrial]['y'], $msg['y']);
+
+                break;
+            case 'end':
+                $stop = true;
+                break;
+        }
+
+        if ($stop) {
+            break;
+        }
+    }
+
+    return $eye_data;
+}
+
 if (isset($_GET['sid'])) {
     $sid = filter_sid($_GET['sid']);
 } else {
@@ -27,50 +69,29 @@ if (isset($_GET['sid'])) {
 
 $pid = filter_input(INPUT_GET, 'pid', FILTER_SANITIZE_NUMBER_INT);
 if ($pid === NULL) {
-    $pid = 1;
+    $pid = -1;
 }
 
-$fn = p_data_filename($sid, $pid);
-if (!file_exists($fn)) {
-    die("Error: no data found for participant $pid (session: $sid)");
-}
+if ($pid === -1) {
+    $eye_data = array();
 
-$pdata = json_decode(file_get_contents($fn), true);
-
-$eye_data = array();
-foreach ($pdata['stimuli'] as $stim) {
-    $eye_data[$stim['name']]['src'] = $stim['src'];
-    $eye_data[$stim['name']]['dest'] = $stim['dest'];
-}
-
-$ctrial = '';
-$stop = false;
-foreach ($pdata['eye_data'] as $msg) {
-    switch ($msg['type']) {
-        case 'trial':
-            $ctrial = $msg['trial'];
-
-            $eye_data[$ctrial]['x'] = array();
-            $eye_data[$ctrial]['y'] = array();
-
-            break;
-        case 'rec':
-            if ($ctrial === '') {
-                continue;
+    foreach (glob(__DIR__ . "/data/s${sid}_p*_data.json") as $fn) {
+        foreach (get_data($fn) as $key => $value) {
+            if (!isset($eye_data[$key])) {
+                $eye_data[$key] = array('x' => array(), 'y' => array());
             }
 
-            array_push($eye_data[$ctrial]['x'], $msg['x']);
-            array_push($eye_data[$ctrial]['y'], $msg['y']);
-
-            break;
-        case 'end':
-            $stop = true;
-            break;
+            array_push($eye_data[$key]['x'], mean($value['x']));
+            array_push($eye_data[$key]['y'], mean($value['y']));
+        }
+    }
+} else {
+    $fn = p_data_filename($sid, $pid);
+    if (!file_exists($fn)) {
+        die("Error: no data found for participant $pid (session: $sid)");
     }
 
-    if ($stop) {
-        break;
-    }
+    $eye_data = get_data($fn);
 }
 ?><html>
     <head>
@@ -96,13 +117,16 @@ foreach ($pdata['eye_data'] as $msg) {
         </style>
     </head>
     <body>
-        <h2>Eye tracking data for participant <?= $pid ?></h2>
+        <h2>Eye tracking data for <?= $pid === -1 ? 'all participants' : "participant $pid" ?></h2>
         <?php
         foreach ($eye_data as $key => $value) {
             echo "<h3>$key</h3>\n";
+
             echo "<ul><li><i>n</i>: " . count($value['x']) . "</li>\n";
+
             $meanx = mean($value['x']);
             echo "<li><i>x</i>: $meanx &plusmn;" . ci95($value['x'], $meanx) . "</li>\n";
+
             $meany = mean($value['y']);
             echo "<li><i>y</i>: $meany &plusmn;" . ci95($value['y'], $meany) . "</li></ul>\n";
         }
