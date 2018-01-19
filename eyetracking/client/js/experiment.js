441dbb6c61c60a208327e0f7ebd56164cda2ca1a
Alex Dewar <a.dewar@sussex.ac.uk>
2018-01-19 11:45:29 +0000

    declare constants as const and remove all-caps naming

--- eyetracking/client/js/experiment.js
+++ eyetracking/client/js/experiment.js
@@ -1,25 +1,25 @@
-var DEBUG = true;
-var REMOTE_URL = 'http://users.sussex.ac.uk/~ad374/eyetracking/server';
-var KEYPRESS_SKIP = DEBUG;
+const debug = true;
+const remote_url = 'http://users.sussex.ac.uk/~ad374/eyetracking/server';
+const keypress_skip = debug;
 
 var date = new Date();
-var SESSION_ID = DEBUG ? '00000000' : date.getFullYear() +
+const session_id = debug ? '00000000' : date.getFullYear() +
         ('0' + (date.getMonth() + 1)).slice(-2) +
         ('0' + date.getDate()).slice(-2);
 
-var ANTS_GAME_DURATION = 180; // seconds
+const ants_game_duration = 180; // seconds
 
-var MAX_CB_DURATION = 60000; // milliseconds
-var CB_TICK_DURATION = 80;
-var CB_SHOW_TICKS = 3;
-var CB_BLANK_TICKS = 1;
-var CB_REPEATS = 2;
-var CB_BLANK_COLOUR = 'lightgray';
+const max_cb_duration = 60000; // milliseconds
+const cb_tick_duration = 80;
+const cb_show_ticks = 3;
+const cb_blank_ticks = 1;
+const cb_repeats = 2;
+const cb_blank_colour = 'lightgray';
 
-var XLABS_DEVELOPER_TOKEN = "2bba2616-cf81-4078-85b9-ddd16749abcb";
-var XLABS_FRAME_SIZE = [800, 600]; // give error if camera res isn't this
+const xlabs_developer_token = "2bba2616-cf81-4078-85b9-ddd16749abcb";
+const xlabs_camera_res = [800, 600]; // give error if camera res isn't this
 
-var IMG_FILES = [
+const img_files = [
     'ants/frame_squish.png',
     'ants/frame0.gif',
     'ants/frame1.gif',
@@ -36,7 +36,7 @@ var IMG_FILES = [
     'change_blindness/4b.jpg'
 ];
 
-var YARBUS_CONDITIONS = [
+const yarbus_conditions = [
     'Your task is to estimate the wealth of the family.',
     'Your task is to guess for how long the man in the image has been away.'
 ];
@@ -48,7 +48,7 @@ var isfullscreen = false;
 var testphase = false;
 var experiment_finished = false;
 
-var cb_blank_slide = {
+const cb_blank_slide = {
     onstart: function () {
         $('.fullscreen').hide();
 
@@ -65,7 +65,7 @@ var cb_blank_slide = {
     }
 };
 
-var submit_data_slide = {
+const submit_data_slide = {
     onstart: function () {
         // hide previous/next buttons for now
         $('#prev_next').hide();
@@ -87,7 +87,7 @@ var submit_data_slide = {
 };
 
 // different sets of slides for experiment
-var intro_slides = [
+const intro_slides = [
     text_slide('<p>Welcome to the eye tracking practical.</p> ' +
             '<p>During this experiment, we will track the positions of your eyes using the webcam in front of you.</p> ' +
             '<p>You will first be given some instructions, then you will play a few games to calibrate the eye tracker, then you will perform some data collection.</p> ' +
@@ -96,14 +96,14 @@ var intro_slides = [
             'You should make sure that your hair is not in your face and if you wear glasses but have only a mild prescription, consider removing them (this is not essential).</p> ' +
             '<p>Most importantly, you should try to keep your head in the same position during both the calibration and testing phases of the experiment.</p>')
 ];
-var check_slides = [
+const check_slides = [
     text_slide('<p>You will now be shown the output of the webcam in order to help you position yourself correctly relative to the webcam.</p>'),
     {
         onstart: check_start,
         onstop: check_end
     }
 ];
-var balloons_slides = [
+const balloons_slides = [
     text_slide('<p>First you will play a game to calibrate the eye tracker.</p>' +
             '<p>The aim is to pop balloons by clicking on them. ' +
             'The eye tracker should learn where you are looking and after a while, your gaze should be visible on the screen as a red circle. ' +
@@ -114,7 +114,7 @@ var balloons_slides = [
         onstop: balloons_end
     }
 ];
-var ants_slides = [
+const ants_slides = [
     text_slide('<p>You will now play another game.</p>' +
             '<p>The aim this time is to squash ants by clicking on them with the mouse. ' +
             'As before, if the system is calibrated correctly you should be able to squash them by looking at them.</p>' +
@@ -124,7 +124,7 @@ var ants_slides = [
         onstop: ants_end
     }
 ];
-var yarbus_slides = [
+const yarbus_slides = [
     yarbus_instructions(),
     {
         onstart: function () {
@@ -136,7 +136,7 @@ var yarbus_slides = [
     },
     img_slide('yarbus.jpg', 30)
 ];
-var cb_slides = [
+const cb_slides = [
     text_slide('<p>You will now do a series of change blindness trials.</p>' +
             '<p>An image will be presented to you (after a delay) and will alternate with a different version of the same image. ' +
             'When you notice the change between the images, press the spacebar to continue to the next trial. ' +
@@ -153,7 +153,7 @@ var cb_slides = [
 ];
 
 // initialise set of "slides" for experiment
-var slides = intro_slides
+const slides = intro_slides
         .concat(check_slides)
         .concat(balloons_slides)
         .concat(ants_slides)
@@ -194,23 +194,23 @@ class Participant {
         this.code = code;
         this.eye_data = [];
 
-        this.yarbus_condition = Math.round(Math.random() * (YARBUS_CONDITIONS.length - 1));
-        console.log('participant assigned to condition ' + this.yarbus_condition + " ('" + YARBUS_CONDITIONS[this.yarbus_condition] + "')");
+        this.yarbus_condition = Math.round(Math.random() * (yarbus_conditions.length - 1));
+        console.log('participant assigned to condition ' + this.yarbus_condition + " ('" + yarbus_conditions[this.yarbus_condition] + "')");
     }
 
     submit_data(success, fail) {
         console.log('submitting data...');
 
         var data = JSON.stringify({
-            sid: SESSION_ID,
+            sid: session_id,
             pid: this.pid,
             code: this.code,
             screen: {width: screen.width, height: screen.height},
             stimuli: stimuli,
             yarbus_condition: this.yarbus_condition,
-            yarbus_conditions: YARBUS_CONDITIONS,
+            yarbus_conditions: yarbus_conditions,
             eye_data: this.eye_data});
-        $.post(REMOTE_URL + '/api/submit_data.php', {data: data},
+        $.post(remote_url + '/api/submit_data.php', {data: data},
                 function (ret) {
                     if (ret.status === "ok") {
                         console.log("data submitted successfully");
@@ -223,7 +223,7 @@ class Participant {
     }
 
     static create(callback) {
-        $.getJSON(REMOTE_URL + '/api/get_participant_id.php?sid=' + SESSION_ID, function (json) {
+        $.getJSON(remote_url + '/api/get_participant_id.php?sid=' + session_id, function (json) {
             participant = new Participant(json.pid, json.code);
             callback();
         }).fail(function () {
@@ -234,10 +234,10 @@ class Participant {
 
 // preload images for image slides
 function preload_images(callback) {
-    var toload = IMG_FILES.length;
+    var toload = img_files.length;
 
-    for (var i in IMG_FILES) {
-        var fn = IMG_FILES[i];
+    for (var i in img_files) {
+        var fn = img_files[i];
 
         var img = new Image();
         imgs[fn] = img;
@@ -264,7 +264,7 @@ function yarbus_instructions() {
         onstart: function () {
             set_instruction_text('<p>This is now the testing phase of the experiment.</p>' +
                     '<p>You now be shown a scene with a family in.</p>' +
-                    '<p>' + YARBUS_CONDITIONS[participant.yarbus_condition] + '</p>' +
+                    '<p>' + yarbus_conditions[participant.yarbus_condition] + '</p>' +
                     '<p>Click next to begin.</p>');
         }
     };
@@ -277,7 +277,7 @@ function img_slide(fn, duration) {
 
             var img = imgs[fn];
 
-            var dest = stimuli[IMG_FILES.indexOf(fn)].dest;
+            var dest = stimuli[img_files.indexOf(fn)].dest;
 
             // draw "slide" image on canvas
             Canvas.clear();
@@ -324,8 +324,8 @@ function cb_slide(num) {
                 trial: prefix
             });
 
-            var dests = [stimuli[IMG_FILES.indexOf(prefix + 'a.jpg')].dest,
-                stimuli[IMG_FILES.indexOf(prefix + 'b.jpg')].dest];
+            var dests = [stimuli[img_files.indexOf(prefix + 'a.jpg')].dest,
+                stimuli[img_files.indexOf(prefix + 'b.jpg')].dest];
 
             // draw "slide" image on canvas
             cbs = [$('#cb0'), $('#cb1')];
@@ -351,15 +351,15 @@ function cb_slide(num) {
                 if (cb_on_flicker) {
                     cbs[cb_which_im].show();
                     cb_on_flicker = false;
-                } else if (++cb_tick === CB_SHOW_TICKS) {
+                } else if (++cb_tick === cb_show_ticks) {
                     cb_tick = 0;
                     cb_on_flicker = true;
                     cbs[cb_which_im].hide();
                     cb_which_im = 1 - cb_which_im;
                 }
-            }, CB_TICK_DURATION);
+            }, cb_tick_duration);
 
-            cb_timeout = setTimeout(slide_next, MAX_CB_DURATION);
+            cb_timeout = setTimeout(slide_next, max_cb_duration);
         },
 
         onstop: function () {
@@ -384,7 +384,7 @@ function ants_start() {
         if (isfullscreen) {
             slide_next();
         }
-    }, ANTS_GAME_DURATION * 1000);
+    }, ants_game_duration * 1000);
 
     set_canvas_click(function (e) {
         ants.onClick(e);
@@ -444,7 +444,7 @@ function on_experiment_end() {
 
 function xlabs_start(callback) {
     xlabs_start_callback = callback;
-    xLabs.setup(on_xlabs_ready, on_xlabs_update, null, XLABS_DEVELOPER_TOKEN);
+    xLabs.setup(on_xlabs_ready, on_xlabs_update, null, xlabs_developer_token);
 }
 
 function on_xlabs_ready() {
@@ -455,7 +455,7 @@ function on_xlabs_ready() {
 
     Check.onXlabsReady();
 
-    if (!DEBUG)
+    if (!debug)
         xLabs.setConfig('calibration.clear', '1');
     xLabs.setConfig('system.mode', 'learning');
     xLabs.setConfig('browser.canvas.paintLearning', '0');
@@ -467,11 +467,11 @@ function on_xlabs_update() {
         xlabs_started = true;
         console.log('xlabs started');
         var camsz = xLabs.getConfig("frame.stream");
-        if (camsz.width == XLABS_FRAME_SIZE[0] && camsz.height == XLABS_FRAME_SIZE[1])
+        if (camsz.width == xlabs_camera_res[0] && camsz.height == xlabs_camera_res[1])
             xlabs_start_callback();
         else
             set_bodyerror("xLabs camera resolution is set to " + camsz.width + "x" + camsz.height +
-                    " &ndash; should be " + XLABS_FRAME_SIZE[0] + "x" + XLABS_FRAME_SIZE[1] + ".");
+                    " &ndash; should be " + xlabs_camera_res[0] + "x" + xlabs_camera_res[1] + ".");
     } else if (mode === 'learning') {
         if (ants.run_game)
             ants.updateGaze();
@@ -508,8 +508,8 @@ function on_all_started(error) {
     if (error)
         throw error;
 
-    for (var i in IMG_FILES) {
-        var fn = IMG_FILES[i];
+    for (var i in img_files) {
+        var fn = img_files[i];
         img = imgs[fn];
 
         // work out the size and position of image on canvas
@@ -543,10 +543,10 @@ function on_all_started(error) {
 }
 
 window.onload = function () {
-    if (DEBUG) {
-        console.log("!!! WARNING: RUNNING IN DEBUG MODE !!!");
+    if (debug) {
+        console.log("!!! WARNING: RUNNING IN debug MODE !!!");
     }
-    console.log('SESSION ID: ' + SESSION_ID);
+    console.log('SESSION ID: ' + session_id);
 
     if (xLabs.extensionVersion() === null) { // no xlabs or not running in chrome
         set_bodyerror("This webpage must be viewed in Google Chrome " +
@@ -561,7 +561,7 @@ window.onload = function () {
             console.log('entering fullscreen mode');
 
             $(document).keypress(function (ev) {
-                if (KEYPRESS_SKIP && ev.key === 'n')
+                if (keypress_skip && ev.key === 'n')
                     slide_next();
                 else if (run_cb && ev.key === ' ') {
                     participant.eye_data.push({
