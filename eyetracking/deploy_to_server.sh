47876f6ebacd9080f3749cbcfca7619e3a4203b6
Alex Dewar <a.dewar@sussex.ac.uk>
2018-04-24 10:33:53 +0100

    quote dir name

--- eyetracking/deploy_to_server.sh
+++ eyetracking/deploy_to_server.sh
@@ -5,4 +5,4 @@ year=$(date +%Y)
 ssh ad374@unix.sussex.ac.uk -C "mkdir -p public_html/eyetracking/{server,data/$year}; chmod a+w public_html/eyetracking/data/{,$year}"
 
 echo Syncing
-rsync -avhr --delete $(dirname $0)/server ad374@unix.sussex.ac.uk:public_html/eyetracking
+rsync -avhr --delete "$(dirname $0)"/server ad374@unix.sussex.ac.uk:public_html/eyetracking
